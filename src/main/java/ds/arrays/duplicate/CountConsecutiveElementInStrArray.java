c4c94be807a874400151b60de9e8d9da195bd6ae
santosh32 <santosh.spring32@gmailc.om>
2018-01-23 20:19:50 +0530

    Change-Id: I22cc5f825165cf308a93bccfef4d7908810dffd8

---
+++ src/main/java/ds/arrays/duplicate/CountConsecutiveElementInStrArray.java
@@ -0,0 +1,33 @@
+package ds.arrays.duplicate;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+//TODO need to work on this
+public class CountConsecutiveElementInStrArray {
+
+	public static void main(String[] args) {
+
+		String inStr = "aaa bb cccc dd aa";
+
+		char[] ch = inStr.toCharArray();
+
+		Map<String, Integer> map = new TreeMap<String, Integer>();
+
+		for (int i = 0; i < ch.length - 1; i++) {
+			int count = 1;
+			String keyStr = String.valueOf(ch[i]);
+			for (int j = i + 1; j < ch.length; j++) {
+				if (ch[i] == ch[j] && i <= j) {
+					keyStr = String.valueOf(ch[j]);
+					count++;
+				}
+			}
+			if (!keyStr.isEmpty()) {
+				map.put(keyStr + String.valueOf(ch[i]), count);
+			}
+
+		}
+		System.out.println(map);
+	}
+}
