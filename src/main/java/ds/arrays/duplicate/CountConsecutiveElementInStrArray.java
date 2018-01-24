19f1dc97f2e175d2cab20f63189e99e3ee16a0ed
santosh32 <santosh.spring32@gmailc.om>
2018-01-24 12:10:57 +0530

    Change-Id: I01dda7cfe94c93448c3750bf6fe4f01e4833559b

--- src/main/java/ds/arrays/duplicate/CountConsecutiveElementInStrArray.java
+++ src/main/java/ds/arrays/duplicate/CountConsecutiveElementInStrArray.java
@@ -1,9 +1,8 @@
 package ds.arrays.duplicate;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
-//TODO need to work on this
 public class CountConsecutiveElementInStrArray {
 
 	public static void main(String[] args) {
@@ -12,19 +11,23 @@ public class CountConsecutiveElementInStrArray {
 
 		char[] ch = inStr.toCharArray();
 
-		Map<String, Integer> map = new TreeMap<String, Integer>();
+		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
 
 		for (int i = 0; i < ch.length - 1; i++) {
 			int count = 1;
 			String keyStr = String.valueOf(ch[i]);
 			for (int j = i + 1; j < ch.length; j++) {
-				if (ch[i] == ch[j] ) {
-					keyStr = String.valueOf(ch[j]);
+				if (ch[i] == ch[j]) {
+					keyStr = keyStr + String.valueOf(ch[j]);
 					count++;
+					i++;
+				} else {
+					break;
 				}
 			}
-			if (!keyStr.isEmpty()) {
-				map.put(keyStr + String.valueOf(ch[i]), count);
+
+			if (!keyStr.equals(" ")) {
+				map.put(keyStr, count);
 			}
 
 		}
