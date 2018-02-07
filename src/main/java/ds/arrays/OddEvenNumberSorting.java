a6c001f76dd934ba1952251eb27a5b3155252622
santosh32 <santosh.spring32@gmailc.om>
2018-02-07 18:42:49 +0530

    Change-Id: Iad9cd335986a010946d7d9bf5440b6873379f399

---
+++ src/main/java/ds/arrays/OddEvenNumberSorting.java
@@ -0,0 +1,32 @@
+package ds.arrays;
+
+import java.util.Arrays;
+
+public class OddEvenNumberSorting {
+
+	static void reverseArray(int inputArray[]) {
+		System.out.println(
+				"Array Before Reverse : " + Arrays.toString(inputArray));
+
+		int temp;
+
+		for (int i = 0, j = inputArray.length - 1; i < (inputArray.length / 2); i++, j--) {
+			temp = inputArray[i];
+			inputArray[i] = inputArray[j];
+			inputArray[j] = temp;
+		}
+
+		System.out.println(
+				"Array After Reverse : " + Arrays.toString(inputArray));
+
+		System.out.println("=========================================");
+	}
+
+	public static void main(String[] args) {
+		reverseArray(new int[] { 4, 5, 8, 9, 10 });
+
+		reverseArray(new int[] { 12, 9, 21, 17, 33, 7 });
+
+		reverseArray(new int[] { 891, 569, 921, 187, 343, 476, 555 });
+	}
+}
---
+++ src/main/java/ds/arrays/SegregateZerosAndOnesFromArray.java
@@ -0,0 +1,38 @@
+package ds.arrays;
+
+import java.util.Arrays;
+
+public class SegregateZerosAndOnesFromArray {
+
+	static void segregateZerosAndOnes(int a[]) {
+
+		System.out.println("Array Before segregate : " + Arrays.toString(a));
+
+		int i = 0, j = a.length - 1;
+
+		while (i < j) {
+			while (a[i] == 0)
+				i++;
+			while (a[j] == 1)
+				j--;
+			if (i < j) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+
+		System.out.println("Array After segregate : " + Arrays.toString(a));
+
+		System.out.println("=========================================");
+	}
+
+	public static void main(String[] args) {
+		segregateZerosAndOnes(new int[] { 1, 0, 0, 1, 0 });
+
+		segregateZerosAndOnes(new int[] { 1, 1, 0, 0, 0, 1 });
+
+	}
+}
