5d4e786955ec7cb7ed46f273c1ab7abcc5b3fe06
santosh32 <santosh.spring32@gmailc.om>
2018-02-07 18:44:42 +0530

    Change-Id: Ice9000dd7825e2dfa426b47ee11885bb59b1a695

--- src/main/java/ds/arrays/OddEvenNumberSorting.java
+++ src/main/java/ds/arrays/OddEvenNumberSorting.java
@@ -4,29 +4,34 @@ import java.util.Arrays;
 
 public class OddEvenNumberSorting {
 
-	static void reverseArray(int inputArray[]) {
-		System.out.println(
-				"Array Before Reverse : " + Arrays.toString(inputArray));
+	static void sorting(int a[]) {
+		System.out.println("Array Before sorting : " + Arrays.toString(a));
 
-		int temp;
+		int i = 0, j = a.length - 1;
 
-		for (int i = 0, j = inputArray.length - 1; i < (inputArray.length / 2); i++, j--) {
-			temp = inputArray[i];
-			inputArray[i] = inputArray[j];
-			inputArray[j] = temp;
+		while (i < j) {
+			while (a[i] == 0)
+				i++;
+			while (a[j] == 1)
+				j--;
+			if (i < j) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
 		}
 
-		System.out.println(
-				"Array After Reverse : " + Arrays.toString(inputArray));
+		System.out.println("Array After sorting : " + Arrays.toString(a));
 
 		System.out.println("=========================================");
 	}
 
 	public static void main(String[] args) {
-		reverseArray(new int[] { 4, 5, 8, 9, 10 });
+		sorting(new int[] { 4, 5, 8, 9, 10 });
 
-		reverseArray(new int[] { 12, 9, 21, 17, 33, 7 });
+		sorting(new int[] { 12, 9, 21, 17, 33, 7 });
 
-		reverseArray(new int[] { 891, 569, 921, 187, 343, 476, 555 });
 	}
 }
