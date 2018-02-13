82e6fc055aa2378df24dba1ecee9a57ec5e9b2ed
Santosh Spring <santosh.spring32@gmail.com>
2018-02-13 22:50:53 +0530

    Change-Id: I21dd2f44c1a58eb03af7e1f7ec6bf8dc015e878d

---
+++ src/main/java/dp/behavioral/template_method/GlassHouse.java
@@ -0,0 +1,14 @@
+package dp.behavioral.template_method;
+
+public class GlassHouse extends HouseTemplate {
+
+	@Override
+	public void buildWalls() {
+		System.out.println("Building Glass Walls");
+	}
+
+	@Override
+	public void buildPillars() {
+		System.out.println("Building Pillars with glass coating");
+	}
+}
