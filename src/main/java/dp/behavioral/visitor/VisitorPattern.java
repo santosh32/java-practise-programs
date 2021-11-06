f4048dc292c7906da1c350d2e5584d2dea9c18ce
amenchu <amaresh_menchu@gap.com>
2021-11-06 19:09:24 +0530

    Added structural and behavioral Patterns

--- src/main/java/dp/behavioral/visitor/VisitorPattern.java
+++ src/main/java/dp/behavioral/visitor/VisitorPattern.java
@@ -82,7 +82,11 @@ class VisitorImpl implements Visitor {
 public class VisitorPattern {
 
     public static void main(String[] args) {
-        Item[] items = new Item[]{new Pen(10, "Parker"), new Pen(5, "Pilot"), new Notebook(50, 150), new Notebook(75, 300)};
+        Item[] items = new Item[]{
+                new Pen(10, "Parker"),
+                new Pen(5, "Pilot"),
+                new Notebook(50, 150),
+                new Notebook(75, 300)};
 
         int total = getTotalPrice(items);
         System.out.println("Total price of items: " + total);
